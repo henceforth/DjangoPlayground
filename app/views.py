4c88538f8448748b6a17e29c1149518c61f4f478
fablange <ext-fabien.lange@nokia.com>
2013-04-09 03:41:58 +0200

    added form stuff, doesnt work yet

--- app/views.py
+++ app/views.py
@@ -2,6 +2,11 @@
 from django.shortcuts import render
 from app.models import Picture, Comments
 from django.template import Context
+from django.forms import ModelForm
+
+class FileForm(ModelForm):
+    class Meta:
+        model = Picture
 
 def index(request):
     pics = Picture.objects.all()
@@ -9,7 +14,17 @@ def index(request):
     return render(request, "index.html", Context({"pics": pics}))
 
 def add(request):
-    
-    return render(request, "add.html", Context())
+    if request.method == 'POST':
+        #form
+        f = FileForm(request.POST)
+        if f.is_valid():
+            f.save()
+            return redirect("index")
+        else:
+            #todo: show errors, reload page
+            pass
+
+    ff = FileForm()
+    return render(request, "add.html", Context({"form":ff}))
 
 
